c24d883dbe81849ee48503d324194d55acf13290
your <your>
2023-08-26 13:27:50 -0700

    main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
-original_str= "Python Programming"
-sub1= original_str [: -12] 
-sub2= (original_str.split()[1]) 
-merge_str = sub2 + sub1
+    original_str= "Python Programming"
+    sub1= original_str [: -12]
+    sub2= (original_str.split()[1]) 
+    merge_str = sub2 + sub1   
+    print (sub2)
+    print (sub1)
+    print (merge_str)
+pass
 
-print (sub2)
-print (sub1)
-print (merge_str)
 if __name__ == '__main__':
     main()
